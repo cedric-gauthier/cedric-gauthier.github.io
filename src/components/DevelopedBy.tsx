a4222d1e3546f1c1841ae042ec80c5874d0b51e9
Cédric GAUTHIER <cedric.gauthier83@gmail.fr>
2024-04-03 06:03:13 +0200

    dsfs

--- src/components/DevelopedBy.tsx
+++ src/components/DevelopedBy.tsx
@@ -4,7 +4,7 @@ const DevelopedBy = () => {
       <span>
         Developed by{" "}
         <a
-          href="https://github.com/flamrdevs"
+          href="https://github.com/cedric-gauthier"
           className="inline-block transition duration-300 hover:-translate-y-px outline-none hover:text-primary-10 focus-visible:text-primary-10"
           target="_blank"
           aria-label="Developer"
